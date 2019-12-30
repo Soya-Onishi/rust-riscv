c249ead7c7060942aec1a926cdddfd919080a8e6
soya <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2019-12-30 16:06:22 +0900

    Fix instruction field truncator to pass compile

--- src/util/instruction.rs
+++ src/util/instruction.rs
@@ -1,5 +1,7 @@
 extern crate bitwise;
 
+use super::status::Status;
+
 use std::fmt;
 use bitwise::*;
 
@@ -41,13 +43,15 @@ impl Instruction {
 
     fn imm(&self) -> Bit {
         match self.encode_type {
-            EncodeType::RType => panic!(make_panic_msg(EncodeType::RType, "imm")),
-            EncodeType::IType => self.raw_code.truncate(31, 20),
+            EncodeType::RType =>
+                panic!(make_panic_msg(EncodeType::RType, "imm")),
+            EncodeType::IType =>
+                self.raw_code.truncate((31, 20)).sign_ext(32),
             EncodeType::SType => {
                 let upper = self.raw_code.truncate((31, 25));
                 let lower = self.raw_code.truncate((11, 7));
 
-                upper.concat(&lower)
+                Bit::concat(vec![&upper, &lower]).sign_ext(32)
             }
             EncodeType::BType => {
                 let bit12 = self.raw_code.truncate(31);
@@ -55,15 +59,22 @@ impl Instruction {
                 let bit10_5 = self.raw_code.truncate((30, 25));
                 let bit4_1 = self.raw_code.truncate((11, 8));
 
+                Bit::concat(vec![&bit12, &bit11, &bit10_5, &bit4_1]).sign_ext(32)
             }
             EncodeType::UType => {
                 let bit31_12 = self.raw_code.truncate((31, 12));
+
+                Bit::concat(vec![&bit31_12, &Bit::new((0, 12))])
             }
             EncodeType::JType => {
                 let bit20 = self.raw_code.truncate(31);
                 let bit19_12 = self.raw_code.truncate((19, 12));
                 let bit11 = self.raw_code.truncate(20);
                 let bit10_1 = self.raw_code.truncate((30, 21));
+                let bit0 = Bit::new((0, 1));
+
+                let bit = Bit::concat(vec![&bit20, &bit19_12, &bit11, &bit10_1, &bit0]);
+                bit.sign_ext(32)
             }
         }
     }
@@ -84,13 +95,19 @@ impl Instruction {
             tpe => panic!(make_panic_msg(tpe, "funct7")),
         }
     }
+
+    pub fn exec(&self, status: &mut Status) {
+        match self.opcode {
+
+        }
+    }
 }
 
 fn make_panic_msg(name: EncodeType, field: &str) -> String {
     format!("{} does not have {} field", name, field)
 }
 
-#[derive(Clone, Copy, Debug, Display)]
+#[derive(Clone, Copy, Debug)]
 enum EncodeType {
     RType,
     IType,
@@ -100,7 +117,13 @@ enum EncodeType {
     JType,
 }
 
-#[derive(Clone, Copy, Debug, Display)]
+impl std::fmt::Display for EncodeType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 enum Opcode {
     LUI,
     AUIPC,
